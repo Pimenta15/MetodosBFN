aef7849093d734848e842e0ea5051f3ac4383bd8
Samuelj-santos <samuel.00000847429@unicap.br>
2024-03-17 17:57:54 -0300

    Update

--- bisseccao.py
+++ bisseccao.py
@@ -3,10 +3,10 @@ import math
 class bisseccao :
 
     @staticmethod
-    def função(a):
-     x = int(a)
-     resultado = ((math.pi*((300/math.cos(x))**2) *0.8)/ (0.5* math.pi*(14**2)*(1 + math.sin(x)-0.5*math.cos(x))) )-1200
-     return resultado
+    def funcao(x):
+        resultado = ((math.pi * (300 / math.cos(x)) ** 2 * 0.8) /
+                     (0.5 * math.pi * 14 ** 2 * (1 + math.sin(x) - 0.5 * math.cos(x)))) - 1200
+        return resultado
 
     @staticmethod
     def calculo(a ,b):
@@ -14,10 +14,18 @@ class bisseccao :
         x = (a+b ) / 2
         erro = abs((a-b) )/2
 
-        while erro >0.01 :
-            fa = bisseccao.função(a);
-            fb = bisseccao.função(b );
-            fx = bisseccao.função(x);
+        while erro >0.001 :
+            fa = bisseccao.funcao(a);
+            fb = bisseccao.funcao(b );
+            fx = bisseccao.funcao(x);
+            
+            print("A:", format_str.format(a))
+            print("B:", format_str.format(b))
+            print("Xi:", format_str.format(x))
+            print("fa:", format_str.format(fa))
+            print("fb:", format_str.format(fb))
+            print("fx:", format_str.format(fx))
+
              
             if fa*fb > 0 :
                erro = abs(x-a)
@@ -29,5 +37,6 @@ class bisseccao :
             else:
                b = x;
 
-            print(format_str.format(x))
+            print("Erro : " , format_str.format(x))
+            print("\n")
             x= (a+b)/2
--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import math
 
 from fPosicao import FalsaPosicao
-from newtonRaphson import newtonRaphson
-from bisseccaoP import Bisseccao 
+#from newtonRaphson import newtonRaphson
+from bisseccaoP import Bisseccao
+
 
 print("Escolha um método:")
 print("1. Método Bissecção")
@@ -17,5 +18,5 @@ if escolha == 1 :
    Bisseccao.calculo(0,(math.pi/25))
 elif escolha == 2:
     FalsaPosicao.falsa_posicao(0,(math.pi/25))
-else:
-    newtonRaphson.nR(math.pi/25)
+#else:
+ #   newtonRaphson.nR(math.pi/25)
